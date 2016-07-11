da2e72e3ca65ca7a92664abdc7abec0f7495c39b
Sergey Nezbritskiy <nezbritskiy.sergey@gmail.com>
2016-07-11 23:22:00 +0300

    add license

--- woocommerce-nova-poshta-shipping.php
+++ woocommerce-nova-poshta-shipping.php
@@ -8,6 +8,23 @@ Author: Sergey Nezbritskiy
 Author URI: http://sergey-nezbritskiy.com
 */
 
+/*  Copyright 2016 Sergey Nezbritskiy (email: nezbritskiy.sergey@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 use plugins\NovaPoshta\classes\AjaxRoute;
 use plugins\NovaPoshta\classes\base\ArrayHelper;
 use plugins\NovaPoshta\classes\Calculator;
