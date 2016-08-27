03602d482bf9126c8a873d3bdfaee58f2e1f2c35
Sergey Nezbritskiy <nezbritskiy.sergey@gmail.com>
2016-08-27 16:53:20 +0300

    update plugins

--- wp-content/plugins/woocommerce/woocommerce.php
+++ wp-content/plugins/woocommerce/woocommerce.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name: WooCommerce
- * Plugin URI: https://www.woothemes.com/woocommerce/
+ * Plugin URI: https://woocommerce.com/
  * Description: An e-commerce toolkit that helps you sell anything. Beautifully.
- * Version: 2.6.2
+ * Version: 2.6.4
  * Author: WooThemes
  * Author URI: https://woothemes.com
  * Requires at least: 4.4
- * Tested up to: 4.5
+ * Tested up to: 4.6
  *
  * Text Domain: woocommerce
  * Domain Path: /i18n/languages/
@@ -35,7 +35,7 @@ final class WooCommerce {
 	 *
 	 * @var string
 	 */
-	public $version = '2.6.2';
+	public $version = '2.6.4';
 
 	/**
 	 * The single instance of the class.
